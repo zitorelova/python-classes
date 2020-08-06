67af2046681238e3642792f68df2db4e6ddd0657
Zito Relova <zitorelova@gmail.com>
2020-08-06 16:19:28 -0700

    Add ground class to flappy

--- flappy.py
+++ flappy.py
@@ -13,8 +13,20 @@ class Obstacle(pygame.sprite.Sprite):
     def update(self):
         self.rect.x = self.rect.x + self.speed_x
 
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height], pygame.SRCALPHA)
+        self.image.fill((0,200,0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 obstacles = pygame.sprite.Group()
 obstacles.add(Obstacle(700, 220, 50, 300))
+
+ground_group = pygame.sprite.Group()
+ground_group.add(Ground(0,400,800,200))
 
 pygame.init()
 
@@ -26,6 +38,8 @@ done = False
 while not done:
     
     obstacles.draw(screen)
+    ground_group.draw(screen)
+    
     pygame.display.flip()
     clock.tick(120)
 pygame.quit()
