5472af39d90b27b6862489a4b0f32b1eb70e9b37
Zito Relova <zitorelova@gmail.com>
2020-08-20 17:40:28 -0700

    Finish flappy bird code

--- flappy.py
+++ flappy.py
@@ -37,6 +37,21 @@ class Bird(pygame.sprite.Sprite):
         self.movement = 0
         pygame.gfxdraw.filled_circle(self.image, 20, 20, 19, (237, 179, 19))
 
+def check_collision(obstacles):
+    for obstacle in obstacles:
+        if bird.rect.colliderect(obstacle):
+            return False
+    
+    if bird.rect.top < -40 or bird.rect.bottom >= 400:
+            return False
+
+    return True
+
+def display_score():
+    score_surface = game_font.render(str(int(score)), True, (255,255,255))
+    score_rect = score_surface.get_rect(center=(400,200))
+    screen.blit(score_surface, score_rect)
+
 obstacles = pygame.sprite.Group()
 
 last_obstacle_time = pygame.time.get_ticks() - 3000
@@ -47,19 +62,29 @@ ground_group.add(Ground(0,400,800,200))
 bird = Bird(100, 200)
 gravity = 0.20
 
+game_active = True
+score = 0
+
 pygame.init()
 
 screen = pygame.display.set_mode([800,600])
 pygame.display.set_caption('Flappy Bird')
 clock = pygame.time.Clock()
+game_font = pygame.font.SysFont('comicsansms', 40)
 
 done = False
 while not done:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird.movement -= 12
+            if event.key == pygame.K_SPACE and game_active == True:
+                bird.movement = 0
+                bird.movement -= 6
+            if event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+                bird = Bird(100, 200)
+                obstacles.empty()
+                score = 0
 
     elapsed_time = pygame.time.get_ticks() - last_obstacle_time
     if elapsed_time > 3000:
@@ -72,12 +97,17 @@ while not done:
     obstacles.draw(screen)
     ground_group.draw(screen)
 
-    bird.movement += gravity
-    bird.rect.centery += bird.movement
-    screen.blit(bird.image, bird.rect)
+    if game_active:
+        bird.movement += gravity
+        bird.rect.centery += bird.movement
+        screen.blit(bird.image, bird.rect)
+        game_active = check_collision(obstacles)
 
-    obstacles.update()
+        obstacles.update()
+        score += 0.01
+        display_score()
 
-    pygame.display.flip()
-    clock.tick(120)
+        pygame.display.flip()
+        clock.tick(120)
+
 pygame.quit()
